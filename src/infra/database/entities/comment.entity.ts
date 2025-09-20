ac2efaae607ceb79c537443bf882fff3495657f4
hash_kim <willom2c@naver.com>
2025-09-21 01:20:32 +0900

    chore: 미사용 파일 삭제

--- src/infra/database/entities/comment.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
-import { Auditable } from './base/auditable';
-
-@Entity()
-export class CommentEntity extends Auditable {
-  @PrimaryGeneratedColumn()
-  id: number;
-}
