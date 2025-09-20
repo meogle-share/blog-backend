7f8c36bec17d9b9447f10595e8670d78e827627f
hash_kim <willom2c@naver.com>
2025-09-20 18:04:22 +0900

    feat: 앱, 데이터베이스, 유즈케이스 모듈 추가

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ApplicationModule } from './application/application.module';
+import { DatabaseModule } from './infra/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './infra/config/env.validation';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate,
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    DatabaseModule,
+    ApplicationModule,
+  ],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/application/application.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { UseCasesModule } from './use-cases/use-cases.module';
+
+@Module({
+  imports: [UseCasesModule],
+  providers: [],
+  exports: [],
+})
+export class ApplicationModule {}
