44a57c16453ce34fefc1af51330a8e51671669a2
hash_kim <willom2c@naver.com>
2025-11-08 18:44:59 +0900

    refactor: 중복된 테스트 제거 - DDD libs 에서 수생하고 있는 테스트는 도메인 단에서 제거

--- src/modules/content/post/domain/post.aggregate.spec.ts
+++ src/modules/content/post/domain/post.aggregate.spec.ts
@@ -23,45 +23,6 @@ describe('Post Aggregate', () => {
       expect(post.getProps().title).toBe(title);
       expect(post.getProps().content).toBe(content);
     });
-
-    it('생성시 자동으로 UUID 형식의 ID가 할당되어야 한다', () => {
-      const post = Post.create({
-        authorId: UserId.generate(),
-        title: PostTitle.from('Test Title'),
-        content: PostContent.from('Test content'),
-      });
-
-      expect(post.id.value).toMatch(
-        /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i,
-      );
-    });
-
-    it('생성시 createdAt과 updatedAt이 설정되어야 한다', () => {
-      const post = Post.create({
-        authorId: UserId.generate(),
-        title: PostTitle.from('Test Title'),
-        content: PostContent.from('Test content'),
-      });
-
-      expect(post.getProps().createdAt).toBeInstanceOf(Date);
-      expect(post.getProps().updatedAt).toBeInstanceOf(Date);
-    });
-
-    it('매번 다른 ID를 가진 Post가 생성되어야 한다', () => {
-      const post1 = Post.create({
-        authorId: UserId.generate(),
-        title: PostTitle.from('Title 1'),
-        content: PostContent.from('Content 1'),
-      });
-
-      const post2 = Post.create({
-        authorId: UserId.generate(),
-        title: PostTitle.from('Title 2'),
-        content: PostContent.from('Content 2'),
-      });
-
-      expect(post1.id.value).not.toBe(post2.id.value);
-    });
   });
 
   describe('from', () => {
@@ -85,86 +46,6 @@ describe('Post Aggregate', () => {
       expect(post.getProps().authorId).toBe(authorId);
       expect(post.getProps().title).toBe(title);
       expect(post.getProps().content).toBe(content);
-    });
-
-    it('특정 ID로 Post를 재구성할 수 있어야 한다', () => {
-      const specificId = PostId.from('01912345-6789-7abc-8666-123456789abc');
-
-      const post = Post.from({
-        id: specificId,
-        props: {
-          authorId: UserId.generate(),
-          title: PostTitle.from('Title'),
-          content: PostContent.from('Content'),
-        },
-      });
-
-      expect(post.id).toBe(specificId);
-      expect(post.id.value).toBe('01912345-6789-7abc-8666-123456789abc');
-    });
-  });
-
-  describe('getProps', () => {
-    it('Post의 모든 속성을 반환해야 한다', () => {
-      const authorId = UserId.generate();
-      const title = PostTitle.from('Title');
-      const content = PostContent.from('Content');
-
-      const post = Post.create({
-        authorId,
-        title,
-        content,
-      });
-
-      const props = post.getProps();
-
-      expect(props).toHaveProperty('id');
-      expect(props).toHaveProperty('authorId');
-      expect(props).toHaveProperty('title');
-      expect(props).toHaveProperty('content');
-      expect(props).toHaveProperty('createdAt');
-      expect(props).toHaveProperty('updatedAt');
-    });
-  });
-
-  describe('ID 비교', () => {
-    it('동일한 ID를 가진 Post는 같은 엔티티로 간주되어야 한다', () => {
-      const postId = PostId.from('01912345-6789-7abc-8777-123456789abc');
-      const post1 = Post.from({
-        id: postId,
-        props: {
-          authorId: UserId.generate(),
-          title: PostTitle.from('Title 1'),
-          content: PostContent.from('Content 1'),
-        },
-      });
-
-      const post2 = Post.from({
-        id: postId,
-        props: {
-          authorId: UserId.generate(),
-          title: PostTitle.from('Title 2'),
-          content: PostContent.from('Content 2'),
-        },
-      });
-
-      expect(post1.id.equals(post2.id)).toBe(true);
-    });
-
-    it('다른 ID를 가진 Post는 다른 엔티티로 간주되어야 한다', () => {
-      const post1 = Post.create({
-        authorId: UserId.generate(),
-        title: PostTitle.from('Title'),
-        content: PostContent.from('Content'),
-      });
-
-      const post2 = Post.create({
-        authorId: UserId.generate(),
-        title: PostTitle.from('Title'),
-        content: PostContent.from('Content'),
-      });
-
-      expect(post1.id.equals(post2.id)).toBe(false);
     });
   });
 });
