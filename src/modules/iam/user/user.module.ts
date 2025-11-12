dd4532cbdcc33ef98447f69e81cf25455d58298b
hash_kim <willom2c@naver.com>
2025-11-12 22:10:36 +0900

    feat: 유저 시딩 기능 추가

--- src/modules/iam/user/user.module.ts
+++ src/modules/iam/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModel } from '@modules/iam/user/infrastructure/user.model';
+import { UserSeeder } from '@modules/iam/user/infrastructure/user.seeder';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserModel])],
+  providers: [UserSeeder],
 })
 export class UserModule {}
