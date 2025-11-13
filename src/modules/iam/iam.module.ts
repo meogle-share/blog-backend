a88b7821440fd1618058323187345c49e8af2f22
hash_kim <willom2c@naver.com>
2025-11-13 22:23:51 +0900

    feat: 인증 모듈 추가

--- src/modules/iam/iam.module.ts
+++ src/modules/iam/iam.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from '@modules/iam/user/user.module';
+import { AuthModule } from '@modules/iam/auth/auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule],
 })
 export class IamModule {}
