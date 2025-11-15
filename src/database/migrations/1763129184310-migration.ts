c84de26ea8a41e3e1956a2a7d34350d45451b492
hash_kim <willom2c@naver.com>
2025-11-15 20:39:58 +0900

    chore: 린팅

--- src/database/migrations/1763129184310-migration.ts
+++ src/database/migrations/1763129184310-migration.ts
@@ -1,10 +1,10 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1763129184310 implements MigrationInterface {
-    name = 'Migration1763129184310'
+  name = 'Migration1763129184310';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             CREATE TABLE "accounts" (
                 "id" uuid NOT NULL,
                 "username" character varying NOT NULL,
@@ -15,7 +15,7 @@ export class Migration1763129184310 implements MigrationInterface {
                 CONSTRAINT "PK_5a7a02c20412299d198e097a8fe" PRIMARY KEY ("id")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "users" (
                 "id" uuid NOT NULL,
                 "nickname" character varying NOT NULL,
@@ -26,7 +26,7 @@ export class Migration1763129184310 implements MigrationInterface {
                 CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "posts" (
                 "id" uuid NOT NULL,
                 "title" character varying NOT NULL,
@@ -37,32 +37,31 @@ export class Migration1763129184310 implements MigrationInterface {
                 CONSTRAINT "PK_2829ac61eff60fcec60d7274b9e" PRIMARY KEY ("id")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "users"
             ADD CONSTRAINT "FK_42bba679e348de51a699fb0a803" FOREIGN KEY ("accountId") REFERENCES "accounts"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "posts"
             ADD CONSTRAINT "FK_c5a322ad12a7bf95460c958e80e" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION
         `);
-    }
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE "posts" DROP CONSTRAINT "FK_c5a322ad12a7bf95460c958e80e"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "users" DROP CONSTRAINT "FK_42bba679e348de51a699fb0a803"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "posts"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "users"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "accounts"
         `);
-    }
-
+  }
 }
