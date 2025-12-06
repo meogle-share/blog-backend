e4ee929e90f8c3111b4469060c3e148324c3acff
hash_kim <willom2c@naver.com>
2025-12-06 17:49:43 +0900

    feat: 병렬 테스트 비활성화

--- test/jest.config.integration.ts
+++ test/jest.config.integration.ts
@@ -8,6 +8,8 @@ const config: Config = {
   testTimeout: 30000,
   globalSetup: '<rootDir>/test/setup/global-setup.ts',
   globalTeardown: '<rootDir>/test/setup/global-teardown.ts',
+  // todo: 병렬 테스트 지원
+  maxWorkers: 1,
 };
 
 export default config;
